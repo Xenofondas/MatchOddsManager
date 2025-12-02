9797286aa5c27e4ac84eee999ad26fb932d34dd9
Xenofontas Amourgis <amourgif@gmail.com>
2025-12-02 21:57:46 +0200

    Initial project setup

---
+++ src/main/java/com/matchoddsmanager/match_odds_manager/MatchOddsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.matchoddsmanager.match_odds_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchOddsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchOddsManagerApplication.class, args);
+	}
+
+}
