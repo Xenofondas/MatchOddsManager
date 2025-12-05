58da9a7a994b19ce8292a7627ead1591af770b3c
Xenofontas Amourgis <amourgif@gmail.com>
2025-12-05 21:07:45 +0200

    added apiResponse

--- src/main/java/com/matchoddsmanager/match_odds_manager/dto/ApiResponse.java
+++ src/main/java/com/matchoddsmanager/match_odds_manager/dto/ApiResponse.java
@@ -1,2 +1,20 @@
-package com.matchoddsmanager.match_odds_manager.dto;public class ApiResponse {
+package com.matchoddsmanager.match_odds_manager.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+
+    private String status;
+    private String message;
+    private T data;
+    private Map<String, String> errors;
 }
